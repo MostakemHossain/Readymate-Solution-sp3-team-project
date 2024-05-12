2c7af0a24d93909f189d3832eea76394f66aa7ae
MostakemHossain <mostakememon123@gmail.com>
2024-05-12 19:56:24 +0600

    hero section added

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ let calcScrollValue=()=>{
     let pos = document.documentElement.scrollTop
     let calcHeight= document.documentElement.scrollHeight-document.documentElement.clientHeight
     let scrollValue= Math.round((pos*100)/calcHeight)
-    console.log(scrollValue)
 
     if(pos>100){
         scrollProgress.style.display="grid"
@@ -26,3 +25,51 @@ let calcScrollValue=()=>{
 
 window.onscroll=calcScrollValue;
 window.onload=calcScrollValue
+
+
+
+let nextBtn = document.querySelector('.next')
+let prevBtn = document.querySelector('.prev')
+
+let slider = document.querySelector('.slider')
+let sliderList = slider.querySelector('.slider .list')
+let thumbnail = document.querySelector('.slider .thumbnail')
+let thumbnailItems = thumbnail.querySelectorAll('.item')
+
+thumbnail.appendChild(thumbnailItems[0])
+
+// Function for next button 
+nextBtn.onclick = function() {
+    moveSlider('next')
+}
+
+
+// Function for prev button 
+prevBtn.onclick = function() {
+    moveSlider('prev')
+}
+
+
+function moveSlider(direction) {
+    let sliderItems = sliderList.querySelectorAll('.item')
+    let thumbnailItems = document.querySelectorAll('.thumbnail .item')
+    
+    if(direction === 'next'){
+        sliderList.appendChild(sliderItems[0])
+        thumbnail.appendChild(thumbnailItems[0])
+        slider.classList.add('next')
+    } else {
+        sliderList.prepend(sliderItems[sliderItems.length - 1])
+        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1])
+        slider.classList.add('prev')
+    }
+
+
+    slider.addEventListener('animationend', function() {
+        if(direction === 'next'){
+            slider.classList.remove('next')
+        } else {
+            slider.classList.remove('prev')
+        }
+    }, {once: true}) // Remove the event listener after it's triggered once
+}
